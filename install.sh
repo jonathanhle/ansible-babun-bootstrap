b3448250b9a82137e50adc5ba2bc76695c19b2db
Sebastián Ramírez <tiangolo@gmail.com>
2015-07-14 09:31:20 -0500

    In install.sh, check if already cloned and pull

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env zsh
 
-git clone https://github.com/tiangolo/ansible-babun-bootstrap.git
+if [ ! -d ansible-babun-bootstrap ]
+ then
+  git clone https://github.com/tiangolo/ansible-babun-bootstrap.git
+else
+  cd ansible-babun-bootstrap
+  git pull
+  cd ..
+fi
+
 source ansible-babun-bootstrap/ansible-babun-bootstrap.sh
